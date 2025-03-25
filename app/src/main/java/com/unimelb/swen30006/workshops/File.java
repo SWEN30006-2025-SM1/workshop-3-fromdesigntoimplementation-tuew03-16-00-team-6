dad3406d37a94d68c2871bd7cc22a065919a9e86
mingchao <mingchaod@student.unimelb.edu.au>
2025-03-25 17:40:04 +1100

    Implemented file class

--- app/src/main/java/com/unimelb/swen30006/workshops/File.java
+++ app/src/main/java/com/unimelb/swen30006/workshops/File.java
@@ -1,7 +1,13 @@
 package com.unimelb.swen30006.workshops;
 
+import java.util.Date;
+
 // Sample private file class to be replaced by your implementaiton
 class File {
+    private Date createdDate;
+    private String fileData;
+    private String fileName;
+
     public String fileType(){
         double num = Math.random();
         if(num<0.5){
@@ -10,4 +16,13 @@ class File {
             return "docx";
         }
     }
+
+    public String content(){
+        return fileName + ": " + fileData;
+    };
+
+    public int fileSize(){
+        return 1;
+    };
+
 }
